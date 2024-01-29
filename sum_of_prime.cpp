ec6d859dd39099eee02bb70ce355bfcca310b3a7
pankajKroy <pankaj.kumar@gemsessence.com>
2024-01-29 13:26:14 +0530

    sum_of_prime error fixed

--- sum_of_prime.cpp
+++ sum_of_prime.cpp
@@ -1,8 +1,9 @@
+//Check whether a number can be expressed as sum of two distinct prime numer or not?
 #include<bits/stdc++.h>
 using namespace std;
 
 bool is_prime(int num){
-  if(num < 1) return false;
+  if(num <= 1) return false;
 
   for(int i=2; i*i <= num ; i++){
     if(num%i == 0) {
@@ -17,7 +18,7 @@ bool is_sum_of_two_prime_num(int n, int &prime1, int &prime2, int start = 2){
  if (n <= 1 || start >= n) 
     return false;
 
-  if (is_prime(start) && is_prime(n - start)) {
+  if (is_prime(start) && is_prime(n - start) && start != (n-start)) {  // prime1 and prime2 must be distinct
     prime1 = start;
     prime2 = n - start;
     return true;
@@ -27,7 +28,7 @@ bool is_sum_of_two_prime_num(int n, int &prime1, int &prime2, int start = 2){
 }
 
 int main(){
-  int num = 10;
+  int num = 6;
   int prime1;
   int prime2;
     if (is_sum_of_two_prime_num(num, prime1, prime2))
